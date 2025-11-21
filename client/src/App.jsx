0649507b0530e6213f83bd9982a0875555ffe8d2
Denislav883 <yanev.denislav@gmail.com>
2025-11-21 22:02:44 +0200

    Show details page with static info

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import Catalog from "./components/catalog/Catalog";
+import Details from "./components/details/Details";
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/games" element={<Catalog />} />
+            <Route path="/games/:detailsId/details" element={<Details />} />
         </Routes>
 
             <Footer />
