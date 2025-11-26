4133068e9d3bba5baa0fd7325a85fa2d5f168940
Denislav883 <yanev.denislav@gmail.com>
2025-11-26 21:12:55 +0200

    Add register

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./components/home/Home";
 import Catalog from "./components/catalog/Catalog";
 import Details from "./components/details/Details";
 import Create from "./components/create/Create";
+import Register from "./components/register/Register";
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
             <Route path="/games" element={<Catalog />} />
             <Route path="/games/:gameId/details" element={<Details />} />
             <Route path="/games/create" element={<Create />} />
+            <Route path="/register" element={<Register />} />
         </Routes>
 
             <Footer />
