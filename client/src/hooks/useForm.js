ae0a4cbe2950e57743b007597ed9868959e2c381
Denislav883 <yanev.denislav@gmail.com>
2025-12-01 20:15:18 +0200

    Create use form hook

---
+++ client/src/hooks/useForm.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+export default function useForm(callback, initialValues) {
+    const [values, setValues] = useState(initialValues);
+
+    const changeHandler = (e) => {
+        setValues(state => ({
+            ...state,
+            [e.target.name]: [e.target.values]
+        }));
+    };
+
+    const formAction = async (formData) => {
+        await callback(values, formData);
+    }
+
+    const register = (fieldName) => {
+        return {
+            name: fieldName,
+            onchange: changeHandler,
+            value: values[fieldName]
+        }
+    }
+
+    return {
+        register,
+        values,
+        changeHandler,
+        formAction
+    }
+}
